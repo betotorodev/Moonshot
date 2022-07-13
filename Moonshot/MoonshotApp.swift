ec80ccfd2314de2a4e83f68fb816dde733b01542
Beto Toro <beto.toro@zemoga.com>
2022-07-13 15:05:01 -0500

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Beto Toro on 13/07/22.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
