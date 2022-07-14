96bf7952caa59beb96086664d8c3b759d8771307
Beto Toro <betotoro0902@gmail.com>
2022-07-14 11:10:46 -0500

    [feat]: create a new extention to load json data from the bundle

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Beto Toro on 14/07/22.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+  let id: String
+  let name: String
+  let description: String
+}
