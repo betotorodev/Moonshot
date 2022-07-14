679958f1ee518c0d11c24ddd52cb904e70cee939
Beto Toro <betotoro0902@gmail.com>
2022-07-14 14:36:30 -0500

    [feat]: add a new way for handling custom colors and the first layout's attempt

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -9,15 +9,58 @@ import SwiftUI
 
 struct ContentView: View {
   
-  let astronauts = Bundle.main.decode("astronauts.json")
+  let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
+  let missions: [Mission] = Bundle.main.decode("missions.json")
+  let columns = [
+    GridItem(.adaptive(minimum: 150))
+  ]
   
-    var body: some View {
-      Text("\(astronauts.count)")
+  var body: some View {
+    NavigationView {
+      ScrollView {
+        LazyVGrid(columns: columns) {
+          ForEach(missions) { mission in
+            NavigationLink {
+              Text("Detail view")
+            } label: {
+              VStack {
+                Image(mission.image)
+                  .resizable()
+                  .scaledToFit()
+                  .frame(width: 100, height: 100)
+                  .padding()
+                
+                VStack {
+                  Text(mission.displayName)
+                    .font(.headline)
+                    .foregroundColor(.white)
+                  Text(mission.formattedLaunchDate)
+                    .font(.caption)
+                    .foregroundColor(.white.opacity(0.5))
+                }
+                .padding(.vertical)
+                .frame(maxWidth: .infinity)
+                .background(.lightBackground)
+              }
+              .clipShape(RoundedRectangle(cornerRadius: 10))
+              .overlay(
+                RoundedRectangle(cornerRadius: 10)
+                  .strokeBorder(.lightBackground)
+              )
+            }
+          }
+        }
+        .padding([.horizontal, .bottom])
+      }
+      .navigationTitle("Moonshot")
+      .background(.darkBackground)
+      .preferredColorScheme(.dark)
     }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
