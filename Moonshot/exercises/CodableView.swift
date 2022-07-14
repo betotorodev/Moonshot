be1033077d5b1bbc5ab19e77bd5297936efa8cde
Beto Toro <betotoro0902@gmail.com>
2022-07-14 09:59:03 -0500

    [feat]: working a little bit deeper with Cosable protocol

---
+++ Moonshot/exercises/CodableView.swift
@@ -0,0 +1,46 @@
+//
+//  CodableView.swift
+//  Moonshot
+//
+//  Created by Beto Toro on 14/07/22.
+//
+
+import SwiftUI
+
+struct User: Codable {
+  let name: String
+  let address: Address
+}
+
+struct Address: Codable {
+  let street: String
+  let city: String
+}
+
+struct CodableView: View {
+  var body: some View {
+    Button("Decode JSON") {
+      let input = """
+          {
+              "name": "Taylor Swift",
+              "address": {
+                  "street": "555, Taylor Swift Avenue",
+                  "city": "Nashville"
+              }
+          }
+          """
+      
+      let data = Data(input.utf8)
+      let decoder = JSONDecoder()
+      if let user = try? decoder.decode(User.self, from: data) {
+        print(user.address.street)
+      }
+    }
+  }
+}
+
+struct CodableView_Previews: PreviewProvider {
+  static var previews: some View {
+    CodableView()
+  }
+}
